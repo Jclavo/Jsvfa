a28112e92fd7474a226602209040616b96fa9d79
jose clavo tafur <jose.tafur>
2023-09-19 14:55:40 -0300

    show methods and its statements

--- src/main/scala/br/unb/cic/JSVFA.scala
+++ src/main/scala/br/unb/cic/JSVFA.scala
@@ -2,17 +2,24 @@ import sootup.java.core.JavaProject
 import sootup.java.core.language.JavaLanguage
 import sootup.java.sourcecode.inputlocation.JavaSourcePathAnalysisInputLocation
 import sootup.core.model.SootMethod
+import sootup.core.model.SootClass
 
 import java.nio.file.Paths
 import java.util.Collections;
 
+
+
+//val view = null;
 @main
 def main(): Unit = {
 
   // show method body
-  val sootMethod = getMethodMainFromClass("HelloWorld")
-  println(sootMethod.getBody())
+//  val sootMethod = getMethodMainFromClass("Calculator")
+//  println(sootMethod.getBody())
+
+  showMethodsFromClass("Calculator")
 }
+
 
 def getMethodMainFromClass(className: String, sourcePath: String = null): SootMethod = {
 
@@ -38,5 +45,37 @@ def getMethodMainFromClass(className: String, sourcePath: String = null): SootMe
 
   // Retrieve method
   sootClass.getMethod(methodSignature.getSubSignature()).get()
+}
+
+//SootClass[Any]
+def showMethodsFromClass(className: String, sourcePath: String = null): Any = {
+
+  val inputLocation = new JavaSourcePathAnalysisInputLocation("src/test/scala/br/unb/cic/sootup/resources")
+
+  // Specify the language of the JavaProject.
+  val language = new JavaLanguage(8)
+
+  // Create a new JavaProject based on the input location
+  val project = JavaProject.builder(language).addInputLocation(inputLocation).build()
+
+  // Create a signature for the class we want to analyze
+  val classType = project.getIdentifierFactory().getClassType(s"br.unb.cic.sootup.resources.$className")
+
+  // Create a view for project, which allows us to retrieve classes
+  val view = project.createView()
+
+  // Retrieve class
+  val sootClass = view.getClass(classType).get()
+
+  sootClass.getMethods().forEach(method => {
+    println(s"analysing method: ${method.getName}")
+    traverse(method)
+  })
+}
+
+def traverse(method: SootMethod): Unit = {
+    method.getBody.getStmts().forEach(stmt => {
+      println(stmt)
+    })
 }
 
---
+++ src/test/scala/br/unb/cic/sootup/resources/Calculator.java
@@ -0,0 +1,30 @@
+package br.unb.cic.sootup.resources;
+
+public class Calculator {
+
+    public void main(String[] var0)
+    {
+        int a = 1;
+        int b = 2;
+        int sum = 0;
+        int sub = 0;
+
+        sum = this.sum(a, b);
+        System.out.println(sum);
+
+        sub = this.sub(a, b);
+        System.out.println(sub);
+    }
+
+    public int sum(int a, int b)
+    {
+        int c = a + b;
+        return c;
+    }
+
+    public int sub(int a, int b)
+    {
+        int c = a - b;
+        return c;
+    }
+}
