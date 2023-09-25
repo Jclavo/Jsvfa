6229c59588cbc57b3756a0c9181584829dc2ddad
jose clavo tafur <jose.tafur>
2023-09-24 21:45:27 -0300

    change way to use enum

--- src/main/scala/br/unb/cic/Graph.scala
+++ src/main/scala/br/unb/cic/Graph.scala
@@ -16,29 +16,34 @@ case class StmtSVFA(stmt: Stmt) {
     case _ => StmtType.InvalidStmt
 }
 
-
-enum NodeType:
-  case SimpleNode
-  case SourceNode
-  case SinkNode
-
-trait NodeSVFA(method: Method, stmt: StmtSVFA, label: NodeType) {
-  def getType: NodeType = label
-}
-
-enum EdgeType:
-  case SimpleEdge
-  case CallEdge
-  case AfterCallEdge
-
-trait EdgeSVFA(to: NodeSVFA, from: NodeSVFA, label: EdgeType)
+enum NodeSVFA(method: Method, stmt: Stmt):
+  def getStmt(): Stmt = stmt
+  case SimpleNode(method: Method, stmt: Stmt) extends NodeSVFA(method, stmt)
+  case SourceNode(method: Method, stmt: Stmt) extends NodeSVFA(method, stmt)
+  case SinkNode(method: Method, stmt: Stmt) extends NodeSVFA(method, stmt)
 
 
-trait GraphSFVA {
+enum EdgeSVFA(to: NodeSVFA, from: NodeSVFA):
+
+  def getNodeTo(): NodeSVFA = to
+  def getNodeFrom(): NodeSVFA = from
+
+  case SimpleEdge(to: NodeSVFA, from: NodeSVFA) extends EdgeSVFA(to, from)
+  case CallEdge(to: NodeSVFA, from: NodeSVFA) extends EdgeSVFA(to, from)
+  case AfterCallEdge(to: NodeSVFA, from: NodeSVFA) extends EdgeSVFA(to, from)
+
+class GraphSFVA {
   private var graph: Set[EdgeSVFA] = Set()
 
   def add(edge: EdgeSVFA): Unit = graph += edge
 
   def convertToDot(): String = "in progress"
+
+  def show(): Unit = graph.foreach(edge => {
+    val to = edge.getNodeTo().getStmt()
+    val from = edge.getNodeFrom().getStmt()
+    println(s"$to -> $from")
+  })
+
 }
 
