c02b7b885bcfa62bcd7b3677056923c6e1203780
jose clavo tafur <jose.tafur>
2023-09-30 17:48:57 -0300

    FullVariableAssignment

--- src/test/scala/br/unb/cic/sootup/rules/RuleCopyTest.scala
+++ src/test/scala/br/unb/cic/sootup/rules/RuleCopyTest.scala
@@ -5,18 +5,25 @@ import org.scalatest.funsuite.AnyFunSuite
 
 class RuleCopyTest extends AnyFunSuite:
 
- test("variable_simple_assignment") {
-   val jsvfa = JSVFA()
-   jsvfa.run("SimpleVariableAssignment", "src/test/scala/br/unb/cic/sootup/resources/JSVFA/rules", "br.unb.cic.sootup.resources.JSVFA.rules")
-//    println(jsvfa.graphSFVA.exportToDot())
-   assert(jsvfa.graphSFVA.edgesTotal() === 3)
- }
-
-  test("variable_complex_assignment") {
+  test("variable_simple_assignment") {
     val jsvfa = JSVFA()
-    jsvfa.run("ComplexVariableAssignment", "src/test/scala/br/unb/cic/sootup/resources/JSVFA/rules", "br.unb.cic.sootup.resources.JSVFA.rules")
-    //    println(jsvfa.graphSFVA.exportToDot())
+    jsvfa.run("SimpleVariableAssignment", "src/test/scala/br/unb/cic/sootup/resources/JSVFA/rules", "br.unb.cic.sootup.resources.JSVFA.rules")
+  //    println(jsvfa.graphSFVA.exportToDot())
     assert(jsvfa.graphSFVA.edgesTotal() === 3)
+  }
+
+   test("variable_complex_assignment") {
+     val jsvfa = JSVFA()
+     jsvfa.run("ComplexVariableAssignment", "src/test/scala/br/unb/cic/sootup/resources/JSVFA/rules", "br.unb.cic.sootup.resources.JSVFA.rules")
+     //    println(jsvfa.graphSFVA.exportToDot())
+     assert(jsvfa.graphSFVA.edgesTotal() === 3)
+   }
+
+  test("variable_full_assignment") {
+    val jsvfa = JSVFA()
+    jsvfa.run("FullVariableAssignment", "src/test/scala/br/unb/cic/sootup/resources/JSVFA/rules", "br.unb.cic.sootup.resources.JSVFA.rules")
+    println(jsvfa.graphSFVA.exportToDot())
+    assert(jsvfa.graphSFVA.edgesTotal() === 9)
   }
 
 
