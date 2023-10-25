97a356ed12f1a5ace4805185f06a8b678b9c356b
jose clavo tafur <jose.tafur>
2023-10-25 09:45:32 -0300

    improve code

--- src/main/scala/br/unb/cic/syntax/Helper.scala
+++ src/main/scala/br/unb/cic/syntax/Helper.scala
@@ -7,15 +7,9 @@ import br.unb.cic.syntax.StmtSVFA.isSourceStmt
 def getSourceStatements(body: Body): Set[Stmt] = {
     var sourceStmt: Set[Stmt] = Set()
     body.getStmts().forEach(stmt => {
-        if (isSourceStmt(stmt)) {
-            sourceStmt += stmt
-        }
-//        isSourceStmt(stmt) match
-//            case true => sourceStmt += stmt
-//            case _ =>
-//        sourceStmt += isSourceStmt(stmt) match
-//            case true => stmt
-//            case _ =>
+        isSourceStmt(stmt) match
+            case true => sourceStmt += stmt
+            case _ =>
     })
     sourceStmt
 }
