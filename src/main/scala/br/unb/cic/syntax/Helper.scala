df9cac9c90438f05cba4f9754807cc1ee9222f3a
jose clavo tafur <jose.tafur>
2023-10-25 09:56:48 -0300

    create method getSourceAndSinkStatements

--- src/main/scala/br/unb/cic/syntax/Helper.scala
+++ src/main/scala/br/unb/cic/syntax/Helper.scala
@@ -1,15 +1,26 @@
 package br.unb.cic.syntax;
 
-import sootup.core.jimple.common.stmt.Stmt;
-import sootup.core.model.Body;
+import sootup.core.jimple.common.stmt.Stmt
+import sootup.core.model.Body
 import br.unb.cic.syntax.StmtSVFA.isSourceStmt
 
-def getSourceStatements(body: Body): Set[Stmt] = {
-    var sourceStmt: Set[Stmt] = Set()
+import scala.collection.mutable
+
+def getSourceAndSinkStatements(body: Body): mutable.HashMap[String, Set[Stmt]] = {
+
+    val sourceStmt: mutable.HashMap[String, Set[Stmt]] = mutable.HashMap()
+
+    sourceStmt("source") = Set()
+    sourceStmt("sink") = Set()
+
     body.getStmts().forEach(stmt => {
         isSourceStmt(stmt) match
-            case true => sourceStmt += stmt
+            case true => sourceStmt("source") += stmt
             case _ =>
     })
     sourceStmt
+}
+
+def getSourceStatements(body: Body): Set[Stmt] = {
+    getSourceAndSinkStatements(body)("source")
 }
