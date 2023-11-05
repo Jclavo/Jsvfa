d81a8e5cd1bdf057bfab9fb0e21fe731a4fb894c
jose clavo tafur <jose.tafur>
2023-11-05 15:47:25 -0300

    create object NodeSVFA

--- src/main/scala/br/unb/cic/syntax/NodeSVFA.scala
+++ src/main/scala/br/unb/cic/syntax/NodeSVFA.scala
@@ -1,5 +1,6 @@
 package br.unb.cic.syntax
 
+import br.unb.cic.syntax.StmtSVFA.{isSinkStmt, isSourceStmt}
 import sootup.core.jimple.common.stmt.Stmt
 import sootup.core.model.SootMethod
 
@@ -8,7 +9,13 @@ enum NodeSVFA(method: SootMethod, stmt: Stmt):
   
   def show(): String = s"${method.getDeclaringClassType.toString}\n<${method.getName}>\n${stmt.toString.replaceAll("\"", "'")}\nline:${stmt.getPositionInfo().getStmtPosition().getFirstLine()}"
 
-
   case SimpleNode(method: SootMethod, stmt: Stmt) extends NodeSVFA(method, stmt)
   case SourceNode(method: SootMethod, stmt: Stmt) extends NodeSVFA(method, stmt)
   case SinkNode(method: SootMethod, stmt: Stmt) extends NodeSVFA(method, stmt)
+
+object NodeSVFA:
+  def createNode(method: SootMethod, stmt: Stmt): NodeSVFA = isSourceStmt(stmt) match
+    case true => NodeSVFA.SourceNode(method, stmt)
+    case _ => isSinkStmt(stmt) match
+      case true => NodeSVFA.SinkNode(method, stmt)
+      case _ => NodeSVFA.SimpleNode(method, stmt)
