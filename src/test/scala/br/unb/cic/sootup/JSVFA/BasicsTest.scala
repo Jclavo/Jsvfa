c6513356c7b8723380728c9c390015fabde7852c
jose clavo tafur <jose.tafur>
2023-11-25 21:56:30 -0300

    complex_source_and_sink

--- src/test/scala/br/unb/cic/sootup/JSVFA/BasicsTest.scala
+++ src/test/scala/br/unb/cic/sootup/JSVFA/BasicsTest.scala
@@ -19,16 +19,16 @@ class BasicsTest extends AnyFunSuite:
     assert(jsvfa.graphSFVA.getAmountOfLeaks === 1)
   }
 
-//  test("complex_source_and_sink") {
-//
-//    val jsvfa = JSVFATest(
-//      "samples.JSVFA.cases.basic.simpleLeak",
-//      "main",
-//      "void",
-//      "src/test/java/samples/JSVFA/cases/basic"
-//    )
-//
-//    jsvfa.run()
-//
-//    assert(jsvfa.graphSFVA.getAmountOfLeaks === 1)
-//  }
+  test("complex_source_and_sink") {
+
+    val jsvfa = JSVFATest(
+      "samples.JSVFA.cases.basic.ComplexLeak",
+      "main",
+      "void",
+      "src/test/java/samples/JSVFA/cases/basic"
+    )
+
+    jsvfa.run()
+
+    assert(jsvfa.graphSFVA.getAmountOfLeaks === 3)
+  }
