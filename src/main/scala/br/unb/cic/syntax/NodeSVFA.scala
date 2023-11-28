9c8796226d9edb35b73b293bd39c1d403b1095a6
jose clavo tafur <jose.tafur>
2023-11-28 18:15:03 -0300

    get method name

--- src/main/scala/br/unb/cic/syntax/NodeSVFA.scala
+++ src/main/scala/br/unb/cic/syntax/NodeSVFA.scala
@@ -7,7 +7,9 @@ enum NodeSVFA(method: SootMethod, stmt: Stmt):
   def getStmt(): Stmt = stmt
 
   def getStmtLine(): Int = stmt.getPositionInfo().getStmtPosition().getFirstLine()
-  
+
+  def getMethodName(): String = method.getName
+
   def show(): String = s"${method.getDeclaringClassType.toString}\n<${method.getName}>\n${stmt.toString.replaceAll("\"", "'")}\nline:${stmt.getPositionInfo().getStmtPosition().getFirstLine()}"
 
   case SimpleNode(method: SootMethod, stmt: Stmt) extends NodeSVFA(method, stmt)
