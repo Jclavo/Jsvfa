131d3b86bf8a7291be0ca11d72169577851e5526
jose clavo tafur <jose.tafur>
2023-11-29 17:54:22 -0300

    add test for paths

---
+++ src/test/scala/br/unb/cic/sootup/path/Path.scala
@@ -0,0 +1,49 @@
+package br.unb.cic.sootup.path
+
+import org.scalatest.funsuite.AnyFunSuite
+import br.unb.cic.sootup.JSVFATest
+
+class Path extends AnyFunSuite:
+
+  test("path1") {
+
+    val jsvfa = JSVFATest(
+      "samples.paths.Path1",
+      "main",
+      "void",
+      "src/test/java/samples/paths"
+    )
+
+    jsvfa.run()
+
+    val sourceNode = jsvfa.graphSFVA.getSourceNodes.head
+    val sinkNode = jsvfa.graphSFVA.getSinkNodes.head
+
+    val g = jsvfa.graphSFVA
+    val r = g.findPaths1(sourceNode, sinkNode)
+
+    assert(r.head.size === 5)
+}
+
+  test("path2") {
+
+    val jsvfa = JSVFATest(
+      "samples.paths.Path2",
+      "main",
+      "void",
+      "src/test/java/samples/paths"
+    )
+
+    jsvfa.run()
+
+    val sourceNode = jsvfa.graphSFVA.getSourceNodes.head
+    val sinkNode = jsvfa.graphSFVA.getSinkNodes.head
+
+    val g = jsvfa.graphSFVA
+
+    val r = g.findPaths1(sourceNode, sinkNode)
+    assert(r.size === 3)
+//    r.foreach(rr => {
+//      println(rr)
+//    })
+  }
